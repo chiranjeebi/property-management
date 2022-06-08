ffa0f50b4898016b0eb5e32ae8d802888c039d7b
chiranjeebi <mallikchiranjeebi@gmail.com>
2022-06-08 23:00:44 +0530

    commit unit test with code coverage

--- src/test/java/com/mycompany/propertymanagement/controller/CalculatorControllerTest.java
+++ src/test/java/com/mycompany/propertymanagement/controller/CalculatorControllerTest.java
@@ -11,9 +11,18 @@ public class CalculatorControllerTest {
     @InjectMocks
     private CalculatorController calculatorController;
 
-    Double num1;
-    Double num2;
-    Double num3;
+   static Double num1;
+    static Double num2;
+    static Double num3;
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("Before All");
+        num1= 11.5 ;
+        num2=10.5 ;
+        num3=5.5 ;
+    }
+
 
 @BeforeEach
     void init(){
@@ -22,33 +31,53 @@ public class CalculatorControllerTest {
      num2=10.5 ;
      num3=5.5 ;
     }
-@AfterEach
-    void init(){
-    num1= null ;
-    num2=null ;
-    num3=null ;
+
+@AfterAll
+    static void afterAll(){
+        System.out.println("After All");
+    num1= null;
+    num2=null;
+    num3=null;
+    }
+
+    @AfterEach
+    void destroy(){
+
         System.out.println("After each");
     }
 
 
 @Test
+@DisplayName("Test addtion for sucess")
+@Disabled
     void testAddFunction_Sucess(){
   Double result =  calculatorController.add(num1,num2,num3);
-
-//expected value is 22
+  //expected value is 22
 //perform assertion
-
-    Assertions.assertEquals( 22,result);
+ Assertions.assertEquals( 22,result);
 }
-
-
-    void testAddFunction_Fail(){
+@Test
+    @DisplayName("Test addtion for failure")
+    void testAddFunction_Failuer(){
         Double result =  calculatorController.add(num1-0.5,num2,num3);
-
-
-
-        Assertions.assertNotEquals( 23,result);
+ Assertions.assertNotEquals( 23,result);
     }
+
+    @Test
+    @DisplayName("Test subtract for num1 & num2")
+    void testSubFunction_num1_gt_num2(){
+        Double result =  calculatorController.subtract(num1,num2);
+        Assertions.assertEquals(1,result);
+    }
+
+    @Test
+    @DisplayName("Test subtract for num1 & num2")
+    void testSubFunction_num1_lt_num2(){
+        Double result =  calculatorController.subtract(num1,num2+1);
+        Assertions.assertEquals(1,result);
+    }
+
+
 }
 
 
